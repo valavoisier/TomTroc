0df55379fbbf09dba16f121abefc4093c83ae476
Valerie Lavoisier <val.lavoisier3@gmail.com>
2025-11-19 00:05:57 +0100

    fix: correction available /status

--- views/books/availableBooks.php
+++ views/books/availableBooks.php
@@ -20,6 +20,9 @@ ob_start(); ?>
                 <a href="<?= ROOT ?>/book/singleBook/<?= $book['id'] ?>" class="card-livre-link">
                     <div class="card-livre">
                         <img src="<?= ROOT ?>/public/img/<?= htmlspecialchars($book['image']) ?>" alt="Livre <?= htmlspecialchars($book['title']) ?>" class="book-image">
+                        <?php if (empty($book['status'])): ?>
+                        <span class="not-available">non dispo.</span>
+                        <?php endif; ?>
                         <div class="card-content">
                             <h3 class="book-title"><?= htmlspecialchars($book['title']) ?></h3>
                             <h4 class="book-subtitle"><?= htmlspecialchars($book['author']) ?></h4>
